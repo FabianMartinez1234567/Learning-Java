726abce27bedcc95b764cff00cec447468cb1150
Fabo xw <55964635+FabianMartinez1234567@users.noreply.github.com>
2020-08-15 01:13:58 -0300

    java

--- Programa1/owo.java
+++ Programa1/owo.java
@@ -2,6 +2,11 @@ public class owo
 {
 	public static void main(String args[])
 	{
-		System.out.println("SOY KHEABRO");	
+		int numero1 = 3;
+		int numero2 = 6;
+		int resultado = 0;
+		resultado = numero1+numero2;
+		System.out.println("El resultado es:  "+ resultado);
+
 	}
 } 
