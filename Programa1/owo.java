56547a6a05787f876fac07b78380ccb9b4a392d0
Fabo xw <55964635+FabianMartinez1234567@users.noreply.github.com>
2020-08-16 00:43:27 -0300

    java

--- Programa1/owo.java
+++ Programa1/owo.java
@@ -2,12 +2,20 @@ public class owo
 {
 	public static void main(String args[])
 	{
-		int numero1 = 3;
-		int numero2 = 6;
-		int numero3 = 4;
+		int numero1 = 10;
+		int numero2 = 10;
+		int numero3 = 11;
 		float promedio = 0;
 		promedio = (numero1 + numero2 + numero3)/3;
 		System.out.println("El resultado es:  "+ promedio);
+		if(promedio>=6)
+		{
+		System.out.println("El alumno aprobo con:"+ promedio);
+		}
+		else
+		{
+		System.out.println("El alumno desaprobo con: "+ promedio);
+		}
 
 	}
 } 
