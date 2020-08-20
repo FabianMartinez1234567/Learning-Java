209c1d5a1243194ec31320b47299ab26d54ed973
Fabo xw <55964635+FabianMartinez1234567@users.noreply.github.com>
2020-08-19 22:13:21 -0300

    Java

--- Programa1/DatosPorTeclado.java
+++ Programa1/DatosPorTeclado.java
@@ -4,32 +4,27 @@ public class DatosPorTeclado
 {
 	public static void main(String args[])
 	{
-		Scanner entrada = new Scanner(System.in);
-		String nombre = "";
-		int clave=0, antiguedad=0;
-		System.out.println("Bienvenido");
-		System.out.println("");
-		System.out.println("");
-		System.out.println("Cual es el nombre del trabajador");
-		nombre=entrada.nextLine();
-		System.out.println("");
-		
-		System.out.println("Cuanto tiempo tiene trabajando el pendejo?");
-		antiguedad=entrada.nextInt();	
-		System.out.println("");
+		int numero1= 5, numero2=3, resultado=0;
+		int parametro=2;
+		switch(parametro){
 
-		System.out.println("Cual es la clave del trabajador?");
-		clave=entrada.nextInt();
-		System.out.println("");
-		
-		if(clave==1){
-		
-		}else if(clave==2){
-		
-		}else if(clave==3){
-		}else
-		{
-			System.out.println("La clave es incorrecta")
+		case 1: resultado=numero1+numero2;
+			System.out.println("La suma es: "+ resultado);
+			break;
+
+		case 2: resultado=numero1-numero2;
+			System.out.println("La resta es: "+ resultado);
+			break;
+
+		case 3: resultado=numero1/numero2;
+			System.out.println("La division es: "+ resultado);
+			break;
+
+		default:System.out.println("Opcion incorrecta");
+			break;
+
+
+
 		}
 	}
 }  
