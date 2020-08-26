7fb05e4522fbdc993e96700c46b693a41ed23b7f
Fabo xw <faboGG.12@gmail.com>
2020-08-25 21:20:36 -0300

    CadenasDeTexto

--- Programa1/DatosPorTeclado.java
+++ Programa1/DatosPorTeclado.java
@@ -4,23 +4,22 @@ public class DatosPorTeclado
 {
 	public static void main(String args[])
 	{
-		int i=0;
-		for (i=1;i<=10;i++)
-		{
-			if(i<10){System.out.print(i+", ");
-			}else{System.out.print(i);}	
-
-		}
-		i=1;
-		System.out.println("");
-		System.out.println("Lo mismo pero con while");
-		while(i<=10){
-		if(i<10){System.out.print(i+", ");
-			}else{System.out.print(i);}
-		i++;
+		String CadenaOriginal = "" , cadenaSub = "";
+		int numCaracteres=0,desde=0, hasta=0;
+		Scanner in = new Scanner(System.in);
 		
-		}
-
+		System.out.print("Introduce una cadena de caracteres: ");
+		CadenaOriginal=in.nextLine();
 		
+		numCaracteres=CadenaOriginal.length();
+		
+		System.out.println("La cadena " + CadenaOriginal + " pose "+ 				numCaracteres + " caracteres.");
+		System.out.println("¿Desde que caracter desea optener la 				cadena?: ");
+		desde = in.nextInt();
+		System.out.println("Hasta que caractes desea obtener la cadena:");
+		hasta = in.nextInt();			
+		
+		cadenaSub= CadenaOriginal.substring(desde,hasta);
+		System.out.print("La nueva cadena es: " + cadenaSub);
 	}
 }  
